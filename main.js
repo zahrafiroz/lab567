4e9056605441e7afae324bb5c2859f2365fff8c6
Zahra <114683477+zahrafiroz@users.noreply.github.com>
2024-02-28 22:14:19 -0800

    Update main.js

--- main.js
+++ main.js
@@ -223,15 +223,15 @@ geojsonFetch();
 function calEarthquakes(currentEarthquakes, currentMapBounds) {
 
     let magnitudesClasses = {
-        1: 0
-        2: 0
-        3: 0
+        1: 0,
+        2: 0,
+        3: 0,
         4: 0,
         5: 0,
-        6: 0
-        7: 0
-        8: 0
-        9: 0
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
         10: 0
     };
     currentEarthquakes.features.forEach(function (d) { // d indicate a feature of currentEarthquakes
